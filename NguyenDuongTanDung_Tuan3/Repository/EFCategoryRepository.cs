214028ace3ec243bd96a92ce8f92375c586b65b1
Dũng Nguyễn <rionguyen028@gmail.com>
2024-03-22 16:09:26 +0700

    Add project files.

---
+++ NguyenDuongTanDung_Tuan3/Repository/EFCategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using NguyenDuongTanDung_Tuan3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NguyenDuongTanDung_Tuan3.Repository
+{
+    public class EFCategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public EFCategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            var product = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<IEnumerable<Category>> GetAllAsync()
+        {
+           return await _context.Categories.ToListAsync();
+        }
+
+        public Task<Category> GetByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ NguyenDuongTanDung_Tuan3/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using NguyenDuongTanDung_Tuan3.Models;
+
+namespace NguyenDuongTanDung_Tuan3.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+ 
+}
+
